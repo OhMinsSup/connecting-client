067c56b4796dcac2f79683aff0096474743e1605
OhMinSeop <mins5190@gmail.com>
2022-04-07 21:41:14 +0900

    add: Home 페이지 내용 추가

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -2,13 +2,15 @@ import React from 'react'
 import styled from 'styled-components'
 
 // icons
-import { Home as HomeIcon } from '@styled-icons/boxicons-solid'
+import { Home as HomeIcon, Cog, PlusCircle } from '@styled-icons/boxicons-solid'
 
 // styles
 import styles from '../assets/styles/modules/home.module.scss'
 import { PageHeader } from '../components/ui/Header'
 
 // components
+import { Link } from 'react-router-dom'
+import CategoryButton from '../components/ui/CategoryButton'
 
 interface HomePageProps {}
 const HomePage: React.FC<HomePageProps> = () => {
@@ -19,7 +21,29 @@ const HomePage: React.FC<HomePageProps> = () => {
           <PageHeader icon={<HomeIcon size={24} />} transparent>
             홈
           </PageHeader>
-          <div className={styles.homeScreen}>??</div>
+          <div className={styles.homeScreen}>
+            <h3>
+              <div>환영합니다!</div>
+              Connecting
+            </h3>
+            <div className={styles.actions}>
+              <Link to="/settings">
+                <CategoryButton action="chevron" icon={<PlusCircle size={32} />} description={`모든 친구들과 멋진 봇들을 초대해서 큰 파티를 열어보세요.`}>
+                  그룹 만들기
+                </CategoryButton>
+              </Link>
+
+              <Link to="/settings">
+                <CategoryButton
+                  action="chevron"
+                  description={`왼쪽 상단에 있는 사용자 아이콘을 마우스 우클릭하거나 이미 홈에 있는 경우 좌클릭할 수도 있습니다.`}
+                  icon={<Cog size={32} />}
+                >
+                  설정열기
+                </CategoryButton>
+              </Link>
+            </div>
+          </div>
         </div>
       </Overlay>
     </div>
